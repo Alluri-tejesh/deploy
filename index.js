d857e34b06893ea76e99987ed939e30272d6ee41
Alluri-tejesh <68057675+Alluri-tejesh@users.noreply.github.com>
2022-11-09 14:25:13 +0530

    index.js is created for pages

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOm from 'react_dom';
+import App from './App';
+ReactDOm.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
